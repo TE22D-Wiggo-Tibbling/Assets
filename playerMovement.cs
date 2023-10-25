0bc2064884c9254ed7aae8d59ce7053b20e5836e
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2023-10-25 09:34:37 +0200

    fesdfsdfsdffsd

--- playerMovement.cs
+++ playerMovement.cs
@@ -12,10 +12,18 @@ public class playerMovement : MonoBehaviour
     [SerializeField]
     float jumpforce = 100;
 
-[SerializeField]
-Transform groundCheck;
+    [SerializeField]
+    Transform groundCheck;
+
+    [SerializeField]
+    float groundRadius = 0.1f;
+
+    [SerializeField]
+    LayerMask groundLayer;
 
     bool mayJump = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +33,8 @@ Transform groundCheck;
     // Update is called once per frame
     void Update()
     {
+        // Debug.Log("hej");
+
         float moveX = Input.GetAxisRaw("Horizontal");
 
         Vector2 movementX = new Vector2(moveX, 0);
@@ -32,23 +42,37 @@ Transform groundCheck;
 
         transform.Translate(movementX * speed * Time.deltaTime);
 
-        if (Input.GetAxisRaw("Jump") > 0 && mayJump == true)
-        {
-            mayJump = false;
-            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        // bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+        Vector3 size = MakeGroundcheckSize();
+        bool isGrounded = Physics2D.OverlapBox(groundCheck.position, size, 0,groundLayer);
 
+
+        if (Input.GetAxisRaw("Jump") > 0 && mayJump == true && isGrounded == true)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
             Vector2 jump = Vector2.up * jumpforce;
+
             rb.AddForce(jump);
+
+            mayJump = false;
         }
 
-        // if (Input.GetAxisRaw("Jump") == 0)
-        // {
-        //     mayJump = true;
-        // }
+
+        if (Input.GetAxisRaw("Jump") == 0)
+        {
+            mayJump = true;
+        }
     }
 
 
-   void OnCollisionEnter2D(Collision2D other) {
-        mayJump=true;
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Vector3 size = MakeGroundcheckSize();
+        Gizmos.DrawWireCube(groundCheck.position, size);
+
     }
+
+    private Vector3 MakeGroundcheckSize() => new Vector3(1.5f, groundRadius);
+
 }
